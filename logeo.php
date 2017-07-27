3eef06c3370939802623952049b75e3af01c8995
briard11 <briard1996@gmail.com>
2017-07-27 11:12:10 -0500

    registro,login y prueba
    prueba con paginas con slider

--- logeo.php
+++ logeo.php
@@ -1,2 +1,14 @@
-echo '<script language="javascript">alert("El stock disponible no es suficiente.");</script>';
-echo '<script language="javascript"> window.location="ventas.php"; </script>';
+<?php  
+include "conexion.php";
+$usuario=$_POST["usuario"];
+$clave=$_POST["clave"];
+
+$query="select usuario, clave from usuarios where usr_name=".usuario." and usr_password=".clave;
+if($resultado = mysqli_query($enlace, query)){
+	echo '<script language="javascript">alert("Ha ingresado con exito.");</script>';
+	echo '<script language="javascript"> window.location="inicio.php"; </script>';
+}else{
+	echo '<script language="javascript">alert("Usuario o clave incorrectas.");</script>';
+	echo '<script language="javascript"> window.location="index.php"; </script>';
+}
+?>
